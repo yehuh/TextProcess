e23eefbb4a77d5799008582eecc8c94c360066c4
chunyuyeh <chunyu.yeh@itec.com.tw>
2022-08-25 15:57:23 +0800

    format modify

--- TextPorcessor.py
+++ TextPorcessor.py
@@ -8,8 +8,8 @@ str_today = time.strftime("%Y-%m-%d", struct_time) # 將時間元組轉
 #str_today = "2022-07-12"
 
 #file_name = (str_today+".txt").strip()
-file_name = "2022-08-01.txt"
-ip_addr = "10.56.40.163"#"10.56.30.120"#"10.56.40.153"
+file_name = "2022-08-25.txt"
+ip_addr = "10.56.40.153"#"10.56.30.120"#"10.56.40.153"
 username='myuser'
 password='a'
 path = "C:\\AGVLog\\system\\"
@@ -18,23 +18,31 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(ip_addr, 22, username, password, timeout=10)
 sftp_client = client.open_sftp()
-remote_file = sftp_client.open(path + file_name)#open("2022-07-02.txt", "r", encoding='UTF-8')
+remote_file = sftp_client.open(path + file_name)
+#remote_file = open(path + file_name, "r", encoding='UTF-8')
+
 output_path = 'remoteFile_' +str_today+".txt"
 out_file = open(output_path, 'w')
-
+the_str = "[CMD : 0x17]"
 line_cnt = 0
 for line in remote_file.readlines():
     out_file.write(line.strip()+"\n")
     line_cnt = line_cnt+1
-    #if "Time Out" not in line:
-    #    continue
-    if line_cnt < 10:
+    if "MCU->PC" not in line:
+        continue
+    if "[CMD : 0x17]" not in line:
+        continue
+    if line_cnt < 1180:
         continue
     #if "parseTagData" not in line:
     #    continue
     #if "Update StateObject.current_NodeNo" in line:
     #    continue
     str_buff = "Line " + str(line_cnt)+": "
-    print((str_buff + line).strip())
+    the_str_id = line.find(the_str)
+    cutted_str = line[the_str_id:]
+    
+    print(('%-12s' % str_buff + cutted_str).strip())
+    #print((str_buff + line).strip())
 remote_file.close()
 out_file.close
